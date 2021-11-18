531df2a796124e7d0af3dcdd2c6d071b38202318
“maozhongbin <maozbmail@163.com>
2021-11-18 22:18:04 +0800

    配置jdk1.8环境

---
+++ src/main/java/text/text.java
@@ -0,0 +1,15 @@
+package text;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Silence_w
+ */
+public class text {
+    public static void main(String[] args) {
+        Map<String,String>map =  new HashMap<>();
+        map.put("1","1");
+
+    }
+}
