5841a485be2775f29bc3dc046542c85a2f22a010
silence <907399030@qq.com>
2021-12-12 23:16:24 +0800

    部分源码和算法

---
+++ src/main/java/arithmetic/day19/offer68_1.java
@@ -0,0 +1,29 @@
+package arithmetic.day19;
+
+public class offer68_1 {
+
+
+      public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+       TreeNode ancestor = root;
+       while(true){
+           if (ancestor.val>p.val && ancestor.val>q.val){
+               ancestor = ancestor.left;
+           }else  if (ancestor.val<p.val && ancestor.val<q.val){
+
+               ancestor = ancestor.right;
+           }else{
+               break;
+           }
+
+       }
+       return ancestor;
+
+    }
+}
